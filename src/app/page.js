e7f71b9963d759c03a8f2180af0b160fd77bcc74
Wesley Ting <wesleyting3@gmail.com>
2025-12-26 14:26:43 +0800

    redirect fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,5 @@
-import Hero from "@/components/Hero";
-import Container from "@/components/Container";
-import GlobalReach from "@/components/GlobalReach";
-import WhoThisIsFor from "@/components/WhoThisIsFor";
-import CinematicVideo from "@/components/CinematicVideo";
+import { redirect } from "next/navigation";
 
-export const metadata = {
-  title: "Home",
-};
-
-export default function Home() {
-  return (
-    <>
-      {/* Hero section with overlay navigation */}
-      <Hero />
-      <GlobalReach />
-      <WhoThisIsFor />
-      <CinematicVideo />
-
-
-    </>
-  );
+export default function RootPage() {
+  redirect("/en");
 }
